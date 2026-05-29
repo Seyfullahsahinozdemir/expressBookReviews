52309167ad532d089cf2634573af1194e61f5c8e
Seyfullah Sahin OZDEMIR <seyfullahsahinozdemir@gmail.com>
2026-05-29 21:58:29 +0300

    .

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,75 +1,86 @@
 const express = require("express");
-let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
+const axios = require("axios");
+
 const public_users = express.Router();
 
-public_users.post("/register", (req, res) => {
-  const { username, password } = req.body;
+const baseURL = "http://localhost:5000";
 
-  if (!username || !password) {
-    return res
-      .status(400)
-      .json({ message: "Username and password are required" });
+// Get all books (async/await)
+public_users.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(`${baseURL}/`);
+    return res.status(200).json(response.data);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
+});
 
-  if (!isValid(username)) {
-    return res.status(400).json({ message: "User already exists" });
+// Get book by ISBN (async/await)
+public_users.get("/isbn/:isbn", async (req, res) => {
+  try {
+    const isbn = req.params.isbn;
+    const response = await axios.get(`${baseURL}/isbn/${isbn}`);
+    return res.status(200).json(response.data);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
-
-  users.push({ username, password });
-
-  return res.status(200).json({ message: "User successfully registered" });
 });
 
-// Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  return res.status(200).send(JSON.stringify(books, null, 4));
-});
-
-// Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
-  const isbn = req.params.isbn;
-  return res.status(200).json(books[isbn]);
-});
-
-// Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+// Get books by author (promise callback)
+public_users.get("/author/:author", (req, res) => {
   const author = req.params.author;
-  let result = {};
 
-  for (let id in books) {
-    if (books[id].author.toLowerCase() === author.toLowerCase()) {
-      result[id] = books[id];
-    }
-  }
+  axios
+    .get(`${baseURL}/`)
+    .then((response) => {
+      const books = response.data;
+      let result = {};
 
-  return res.status(200).json(result);
+      for (let id in books) {
+        if (books[id].author.toLowerCase() === author.toLowerCase()) {
+          result[id] = books[id];
+        }
+      }
+
+      return res.status(200).json(result);
+    })
+    .catch((err) => {
+      return res.status(500).json({ message: err.message });
+    });
 });
 
-// Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+// Get books by title (promise callback)
+public_users.get("/title/:title", (req, res) => {
   const title = req.params.title;
-  let result = {};
 
-  for (let id in books) {
-    if (books[id].title.toLowerCase() === title.toLowerCase()) {
-      result[id] = books[id];
-    }
-  }
+  axios
+    .get(`${baseURL}/`)
+    .then((response) => {
+      const books = response.data;
+      let result = {};
 
-  return res.status(200).json(result);
+      for (let id in books) {
+        if (books[id].title.toLowerCase() === title.toLowerCase()) {
+          result[id] = books[id];
+        }
+      }
+
+      return res.status(200).json(result);
+    })
+    .catch((err) => {
+      return res.status(500).json({ message: err.message });
+    });
 });
 
-//  Get book review
-public_users.get("/review/:isbn", function (req, res) {
-  const isbn = req.params.isbn;
-
-  if (books[isbn]) {
-    return res.status(200).json(books[isbn].reviews);
+// Get reviews by ISBN (async/await)
+public_users.get("/review/:isbn", async (req, res) => {
+  try {
+    const isbn = req.params.isbn;
+    const response = await axios.get(`${baseURL}/review/${isbn}`);
+    return res.status(200).json(response.data);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
-
-  return res.status(404).json({ message: "Book not found" });
 });
 
 module.exports.general = public_users;
